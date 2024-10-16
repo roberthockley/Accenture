da7d157383794048244bd99a36de8660cba14e1a
Robert Hockley <robert.hockley@accenture.com>
2024-10-16 15:10:48 +0800

    s3q

--- Terraform/Connect/s3_transcripts_recordings.tf
+++ Terraform/Connect/s3_transcripts_recordings.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "transcripts" {
   bucket = "${var.project.tla}-recordings-transcripts"
 }
+
+resource "aws_s3_bucket" "q" {
+  bucket = "${var.project.tla}-connect-amazonq"
+}
