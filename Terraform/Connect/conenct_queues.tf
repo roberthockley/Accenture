63a75236e45326f01a10863630524f9c09f2695a
Robert Hockley <robert.hockley@accenture.com>
2024-10-16 10:48:34 +0800

    F

--- Terraform/Connect/conenct_queues.tf
+++ Terraform/Connect/conenct_queues.tf
@@ -8,4 +8,5 @@ resource "aws_connect_queue" "sales" {
     outbound_caller_id_name      = "Accenture Song"
     #outbound_caller_id_number_id = "12345678-abcd-1234-abcd-123456789012"
     outbound_flow_id             = data.aws_connect_contact_flow.default_outbound
+  }
 }
