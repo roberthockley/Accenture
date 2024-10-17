200bf6ccdf57337657010731c43153aea80c75fa
Robert Hockley <robert.hockley@accenture.com>
2024-10-17 10:38:55 +0800

    IAM

---
+++ Terraform/Connect/iam.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "iam_role_lambda_ssm" {
+  name = "RoleForLamdaOutages"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_ssm" {
+  role       = aws_iam_role.iam_role_lambda_ssm.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
--- Terraform/Connect/lambda.tf
+++ Terraform/Connect/lambda.tf
@@ -12,8 +12,8 @@ resource "aws_lambda_function" "lambda_outages" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "outages.zip"
-  function_name = var.lambda_functions.lambda_spvTransferCall_name
-  role          = var.input.iam_role_lambda_outages
+  function_name = "outages"
+  role          = aws_iam_role.iam_role_lambda_ssm.arn
   handler       = "index.handler"
   publish       = true
   layers        = [aws_lambda_layer_version.lambda_layer_ssm.arn] 
